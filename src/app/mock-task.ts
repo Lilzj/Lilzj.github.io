4fe2666b51b90cbd6d8374627addd6d8bc877579
lilzj <joshua.gbogodor@decagon.dev>
2021-08-05 15:18:32 +0100

    added the task components and fontawesome package

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'August 3rd at 4:43pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'August 3rd at 4:43pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'August 3rd at 4:43pm',
+    reminder: false,
+  },
+];
