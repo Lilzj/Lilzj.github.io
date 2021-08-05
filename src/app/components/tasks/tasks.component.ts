a61e26f066badb9ea80e4e81c8e49add47fb1f6c
lilzj <joshua.gbogodor@decagon.dev>
2021-08-05 16:01:06 +0100

    added a json server for the backend to handle the database and some http requeat

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-task';
+import { TaskService } from '../../services/task.service';
 
 
 @Component({
@@ -9,11 +9,12 @@ import {TASKS} from '../../mock-task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
 }
